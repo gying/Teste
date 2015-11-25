16f10b7b989b69a4ca9bc3970b92fa684006ca06
xtwldx <441041956@qq.com>
2015-11-25 17:51:17 +0800

    change11.25PM4

--- Teste/ViewController/AddDishesViewController.h
+++ Teste/ViewController/AddDishesViewController.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface AddDishesViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *dishName;
+@property (weak, nonatomic) IBOutlet UITextField *dishPrice;
+@property (weak, nonatomic) IBOutlet UITextField *dishRating;
+@property (weak, nonatomic) IBOutlet UITextField *chef;
+@property (weak, nonatomic) IBOutlet UITextView *dishDesc;
+
 
 @end
